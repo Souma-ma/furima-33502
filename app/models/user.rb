29fd474466cddc0b463c3fad768a5fd8a00f9d50
Souma-ma <somamatsutani07@gmail.com>
2021-01-20 16:30:11 +0900

    バリデーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   validates :email, uniqueness: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} #半角英数混合
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
   validates :birth_day, presence: true
-  validates :password, format: {with: /\A[-]?[0-9]+(\.[0-9]+)?\z/} #半角英数混合
 end
