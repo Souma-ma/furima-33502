e0c17398ad319f0d8f79526271c8a73288fb639b
Souma-ma <somamatsutani07@gmail.com>
2021-01-20 11:36:59 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :email, uniqueness: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
   validates :birth_day, presence: true
+  validates :password, format: {with: /\A[-]?[0-9]+(\.[0-9]+)?\z/} #半角英数混合
 end
