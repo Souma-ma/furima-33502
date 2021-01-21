a1a868abf54ccd69217b4709374c656023acb27a
Souma-ma <somamatsutani07@gmail.com>
2021-01-21 11:17:44 +0900

    with_optionsで共通したオプションをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_day
+  end
   validates :email, uniqueness: true
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} #半角英数混合
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
-  validates :birth_day, presence: true
+  validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
+  validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/} #全角入力必須
+  validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
+  validates :first_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/} #全角カナ入力必須
 end
