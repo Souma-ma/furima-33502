459c57b85b3375ade325cebd2e2b6566c4d71522
Souma-ma <somamatsutani07@gmail.com>
2021-01-22 12:27:34 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,17 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :region
   belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :name
+    validates :contents
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :postage_id
+    validates :region_id
+    validates :shipping_date_id
+  end
 end
