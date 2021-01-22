6cdc76ef305ce8b422a1bdd1a094f41e23eaa556
Souma-ma <somamatsutani07@gmail.com>
2021-01-22 12:19:48 +0900

    アソシエーションを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash:Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
 end
