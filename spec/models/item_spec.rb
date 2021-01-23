997e4ae5c6f9d4bb4acd5ae45e2d5ffc3651b865
Souma-ma <somamatsutani07@gmail.com>
2021-01-23 14:54:20 +0900

    商品出品できる時のテスト(example)を生成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  descrive "商品出品機能" do
+    context '商品出品できるとき' do
+      it "imageとnameとcontentsとcategory_idとstate_idとpostage_idとregion_idとshipping_idとpriceがあれば出品できる" do
+        
+      end
+      it "category_idが1以外なら登録できる" do
+        
+      end
+      it "state_idが1以外なら登録できる" do
+        
+      end
+      it "postage_idが1以外なら登録できる" do
+        
+      end
+      it "region_idが1以外なら登録できる" do
+        
+      end
+      it "shipping_date_idが1以外なら登録できる" do
+        
+      end
+      it "priceが300円以上なら登録できる" do
+        
+      end
+      it "priceが9999999円以下なら登録できる" do
+        
+      end
+    end
+    context '商品出品できないとき' do
+      
+    end
+  end
 end
