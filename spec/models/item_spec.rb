9b28d73c442850907441a4b148ab622f3f34f9fe
Souma-ma <somamatsutani07@gmail.com>
2021-01-23 15:04:15 +0900

    商品出品ができないときのテスト(example)を生成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,30 +10,65 @@ RSpec.describe Item, type: :model do
       it "imageとnameとcontentsとcategory_idとstate_idとpostage_idとregion_idとshipping_idとpriceがあれば出品できる" do
         
       end
-      it "category_idが1以外なら登録できる" do
+      it "category_idが1以外なら出品できる" do
         
       end
-      it "state_idが1以外なら登録できる" do
+      it "state_idが1以外なら出品できる" do
         
       end
-      it "postage_idが1以外なら登録できる" do
+      it "postage_idが1以外なら出品できる" do
         
       end
-      it "region_idが1以外なら登録できる" do
+      it "region_idが1以外なら出品できる" do
         
       end
-      it "shipping_date_idが1以外なら登録できる" do
+      it "shipping_date_idが1以外なら出品できる" do
         
       end
-      it "priceが300円以上なら登録できる" do
+      it "priceが300円以上なら出品できる" do
         
       end
-      it "priceが9999999円以下なら登録できる" do
+      it "priceが9999999円以下なら出品できる" do
         
       end
     end
     context '商品出品できないとき' do
-      
+      it "商品画像が無ければ出品できない" do
+        
+      end
+      it "nameが空では出品できない" do
+        
+      end
+      it "contentsが空では出品できない" do
+        
+      end
+      it "category_idが1なら出品できない" do
+        
+      end
+      it "state_idが1なら出品できない" do
+        
+      end
+      it "postage_idが1なら出品できない" do
+        
+      end
+      it "region_idが1なら出品できない" do
+        
+      end
+      it "shipping_date_idが1なら出品できない" do
+        
+      end
+      it "priceが空では出品できない" do
+        
+      end
+      it "priceが全角数字で入力されると出品できない" do
+        
+      end
+      it "priceが300円未満なら出品できない" do
+        
+      end
+      it "priceが9999999円以上なら出品できない" do
+        
+      end
     end
   end
 end
