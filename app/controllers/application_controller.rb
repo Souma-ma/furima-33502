21c6c3561d0f71ad6070d25aa69f939c496c5820
Souma-ma <somamatsutani07@gmail.com>
2021-01-23 16:34:19 +0900

    berere_actionの記述場所変更、createアクション内を編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:index]
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
