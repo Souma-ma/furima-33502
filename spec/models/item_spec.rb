680dc2ab74478352b58f4b51360f5f89e8c392e3
Souma-ma <somamatsutani07@gmail.com>
2021-01-23 16:17:04 +0900

    不要なファイル、テスト、コードなどを削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,14 +10,6 @@ RSpec.describe Item, type: :model do
       it 'imageとnameとcontentsとcategory_idとstate_idとpostage_idとregion_idとshipping_idとpriceがあれば出品できる' do
         expect(@item).to be_valid
       end
-      it 'priceが300円以上なら出品できる' do
-        @item.price = 300
-        expect(@item).to be_valid
-      end
-      it 'priceが9999999円以下なら出品できる' do
-        @item.price = 9_999_999
-        expect(@item).to be_valid
-      end
     end
     context '商品出品できないとき' do
       it '商品画像が無ければ出品できない' do
