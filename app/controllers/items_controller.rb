177970dc9bd06a8d112f9e0689e7db84f4ceefc8
Souma-ma <somamatsutani07@gmail.com>
2021-01-24 15:13:23 +0900

    同じ記述をメソッドにまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :get_item, only: [:show,:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -31,7 +31,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -42,5 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :contents, :category_id, :state_id, :postage_id, :region_id, :shipping_date_id,
                                  :price).merge(user_id: current_user.id)
+  end
+  
+  def get_item
+    @item = Item.find(params[:id])
   end
 end
