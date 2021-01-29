521477545f684a891084f1146f2f0411ead08005
Souma-ma <somamatsutani07@gmail.com>
2021-01-29 16:21:39 +0900

    購入機能、createアクションを編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,9 +5,10 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @purchase_item = PurchaseItem.new(purchase_item_params)
     if @purchase_item.valid?
+       pay_item
        @purchase_item.save
        redirect_to root_path
     else
@@ -19,5 +20,14 @@ class PurchasesController < ApplicationController
   private
   def purchase_item_params
     params.require(:purchase_item).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_item_params[:token],
+        currency: 'jpy'
+      )
   end
 end
