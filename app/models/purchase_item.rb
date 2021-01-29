a7c7b46ed29ee513e81ceecf5163ccfd253b9d15
Souma-ma <somamatsutani07@gmail.com>
2021-01-29 16:20:05 +0900

    tokenにバリデーションを追加

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -8,6 +8,7 @@ class PurchaseItem
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid." }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1 }
