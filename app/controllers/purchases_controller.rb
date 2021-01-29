97a8f33903e985da7c166f18b81819f64a536cab
Souma-ma <somamatsutani07@gmail.com>
2021-01-29 20:57:42 +0900

    item_setメソッドで定義しているため、削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -38,14 +38,12 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_root
-    @item = Item.find(params[:item_id])
     if @item.user_id == current_user.id
       redirect_to root_path 
     end
   end
 
   def direct_top
-    @item = Item.find(params[:item_id])
     redirect_to root_path if @item.purchase.present?
   end
 
