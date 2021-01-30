563014c8a66e57b9590993701037a0ce7e1b8588
Souma-ma <somamatsutani07@gmail.com>
2021-01-30 14:20:07 +0900

    item_idとuser_idの取得方法を変更

--- spec/models/purchase_item_spec.rb
+++ spec/models/purchase_item_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PurchaseItem, type: :model do
   before do
-    @purchase_item = FactoryBot.build(:purchase_item)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_item = FactoryBot.build(:purchase_item, user_id: user.id, item_id: item.id)
   end
 
   context '商品を購入できるとき' do
